67fcf5f4603005e7b0d01aad6d6ecf1bb89a5a63
Rohan Sadhwani <rohansadhwani5@gmail.com>
2025-03-31 21:13:46 +0530

    PDF multer implemented

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const path = require("path");
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 });
+
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+    console.log(req.files)
+    res.send({data:req.files})
+  })
 
 app.listen(port, () => {
   console.log(`PDF merger app listening on port http://localhost:${port}`);
